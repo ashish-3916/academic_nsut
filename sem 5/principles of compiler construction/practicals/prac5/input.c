413af9e6915b5c95ba5128d197711c1e2aa8905f
ashish-3916 <dangiashish1618@gmail.com>
2021-12-07 09:18:02 +0530

    fdgs

--- sem 5/principles of compiler construction/practicals/prac5/input.c
+++ sem 5/principles of compiler construction/practicals/prac5/input.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
 #define PI 3.14
-/*
-To run:
-yacc -d parser.y
-lex parser.l
-gcc -o a.out y.tab.c yy.lex.c -ll
-./a.out < input.c
-*/
-
 struct inp
 {
     int a;
 };
-
 int check(int a, int b)
 {
     return (a > b);
 }
-
-/* Sample code */
 int main()
 {
     struct inp ab;
